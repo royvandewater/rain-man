48c75b5786c9381d3dcf874888979de2159acb3e
Roy van de Water <me@royvandewater.com>
2011-10-02 15:25:20 -0700

    Google parser parses city and date

--- Rain Man Test/src/com/royvandewater/rainman/test/util/GoogleForecastParserTest.java
+++ Rain Man Test/src/com/royvandewater/rainman/test/util/GoogleForecastParserTest.java
@@ -2,6 +2,8 @@ package com.royvandewater.rainman.test.util;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.ParseException;
+import java.util.Date;
 
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -20,17 +22,22 @@ public class GoogleForecastParserTest extends InstrumentationTestCase {
 
 	final private String CLASS_NAME = "class com.royvandewater.rainman.util.GoogleForecastParser";
 	final private String FIXTURE    = "google_forecast.xml";
+	
+	private static final String EXPECTED_LOCATION = "Mountain View, CA";
+	private static final Date EXPECTED_DATE = new Date(1315889760000L);
+	
 	private InputStream sampleXmlInputStream;
+	private GoogleForecastParser forecastParser;
 	
 	@Override
 	protected void setUp() throws Exception {
 		Context context = getInstrumentation().getContext();
 		AssetManager assetManager = context.getAssets();
 		this.sampleXmlInputStream = assetManager.open(FIXTURE);
+		this.forecastParser = new GoogleForecastParser();
 	}
 	
 	public void testConstructor() {
-		GoogleForecastParser forecastParser = new GoogleForecastParser();
 		assertEquals(CLASS_NAME, forecastParser.getClass().toString());
 	}
 	
@@ -39,9 +46,16 @@ public class GoogleForecastParserTest extends InstrumentationTestCase {
 		assertTrue(sampleXml.startsWith("<xml_api_reply version=\"1\">"));
 	}
 	
+	public void testParseDate() throws ParseException {
+		Date date = forecastParser.parseDate("2011-09-13 04:56:00 +0000");
+		assertEquals(EXPECTED_DATE, date);
+	}
+	
 	public void testParse() throws XmlPullParserException, ParserConfigurationException, SAXException, IOException {
-		GoogleForecastParser forecastParser = new GoogleForecastParser();
+		
 		Forecast forecast = forecastParser.parse(sampleXmlInputStream);
-		assertEquals("Mountain View, CA", forecast.getLocation());
+		
+		assertEquals(EXPECTED_LOCATION, forecast.getLocation());
+		assertEquals(EXPECTED_DATE, forecast.getDate());
 	}
 }
--- Rain Man/src/com/royvandewater/rainman/util/GoogleForecastParser.java
+++ Rain Man/src/com/royvandewater/rainman/util/GoogleForecastParser.java
@@ -2,11 +2,15 @@ package com.royvandewater.rainman.util;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 import org.xmlpull.v1.XmlPullParserException;
@@ -15,46 +19,99 @@ import com.royvandewater.rainman.models.Forecast;
 
 public class GoogleForecastParser extends DefaultHandler {
 
+	public static String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss Z";
+
+	public static String DATA = "data";
 	
-	static final String CITY = "city";
-	static final String CURRENT_DATE_TIME = "current_date_time";
-	static final String CURRENT_CONDITION = "current_conditions";
-	static final String FORECAST_CONDITIONS = "forecast_conditions";
-	static final String CONDITION = "condition";
-	static final String HUMIDITY = "humidity";
-	static final String DAY_OF_WEEK = "day_of_week";
-	static final String LOW = "low";
-	static final String HIGH = "high";
-	
+	public enum Tag {
+		/* Sections */
+		FORECAST_INFORMATION, CURRENT_CONDITIONS, FORECAST_CONDITIONS,
+		/* Tags */
+		WEATHER, CITY, CURRENT_DATE_TIME, CONDITION, HUMIDITY, DAY_OF_WEEK, LOW, HIGH,
+
+		NOVALUE;
+
+		public static Tag toTag(String str) {
+			try {
+				return valueOf(str.toUpperCase());
+			} catch (Exception ex) {
+				return NOVALUE;
+			}
+		}
+	}
+
 	private Forecast forecast;
 	private StringBuilder builder;
-	
-	public Forecast parse (InputStream inputStream) throws XmlPullParserException, ParserConfigurationException, SAXException, IOException {
+
+	private boolean inForecastInformation, inCurrentConditions,
+			inForecastConditions;
+
+	public Forecast parse(InputStream inputStream)
+			throws XmlPullParserException, ParserConfigurationException,
+			SAXException, IOException {
 		SAXParserFactory factory = SAXParserFactory.newInstance();
 		SAXParser parser = factory.newSAXParser();
 		parser.parse(inputStream, this);
-		
-		return new Forecast();
+
+		return forecast;
 	}
-	
+
+	@Override
+	public void startDocument() throws SAXException {
+		super.startDocument();
+		this.builder = new StringBuilder();
+		this.forecast = new Forecast();
+	}
+
+	@Override
+	public void startElement(String uri, String localName, String qName,
+			Attributes attributes) throws SAXException {
+		super.startElement(uri, localName, qName, attributes);
+
+		switch (Tag.toTag(localName)) {
+		case CITY:
+			forecast.setLocation(attributes.getValue(DATA));
+			break;
+		case CURRENT_DATE_TIME:
+			Date date = parseDate(attributes.getValue(DATA));
+			forecast.setDate(date);
+			break;
+		case FORECAST_INFORMATION:
+			inForecastInformation = true;
+			break;
+		default:
+			break;
+		}
+	}
+
 	@Override
 	public void characters(char[] ch, int start, int length)
 			throws SAXException {
 		super.characters(ch, start, length);
 		builder.append(ch, start, length);
 	}
-	
+
 	@Override
 	public void endElement(String uri, String localName, String qName)
 			throws SAXException {
 		super.endElement(uri, localName, qName);
 		
-		if (this.forecast != null) {
-			if (localName.equalsIgnoreCase(CITY)) {
-				forecast.setLocation(builder.toString());
-			}
-			
-			builder.setLength(0);
+		switch(Tag.toTag(localName)) {
+		case FORECAST_INFORMATION:
+			inForecastInformation = false;
+			break;
 		}
+
+		builder.setLength(0);
+	}
+
+	public Date parseDate(String string) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+		try {
+			return dateFormat.parse(string);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
